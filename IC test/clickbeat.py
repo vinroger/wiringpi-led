cbb9ec6bfe673563852403b54c021be10fba7fb3
underpass raspberry pi <underpass@dti.com>
2022-04-10 23:04:14 +0800

    update function

--- IC test/clickbeat.py
+++ IC test/clickbeat.py
@@ -1,22 +1,34 @@
 import wiringpi as wiringpi
 from time import sleep
-from pydub import AudioSegment
-from pydub.playback import play
 import copy
 import os
 import datetime
 import time
 import threading
+import pygame
+import time
 #import time
 
-pin_base = 65       # lowest available starting number is 65
-ic1_addr = 0x20     # A0, A1, A2 pins all wired to GND
-ic2_addr = 0x21     # A0, A1, A2 pins all wired to GND
-ic3_addr = 0x22     # A0, A1, A2 pins all wired to GND
+pin_base = 65       # lowest available starting number is 65  
+i2c_addr = 0x20     # A0, A1, A2 pins all wired to GND  
+i2c2_addr = 0x21 
+i2c3_addr = 0x23
+i2c4_addr = 0x24
+i2c5_addr = 0x25  # A0, A1, A2 pins all wired to GND
+
+
+
+# DECLARATION of wiring Pi
+wiringpi.wiringPiSetup()                    # initialise wiringpi  
+wiringpi.mcp23017Setup(pin_base,i2c_addr)     # set up the pins and i2c address 
+wiringpi.mcp23017Setup(81,i2c2_addr) 
+wiringpi.mcp23017Setup(97,i2c3_addr)
+wiringpi.mcp23017Setup(113,i2c4_addr)
+wiringpi.mcp23017Setup(129,i2c5_addr)
 
 
 buttonPinLocation = [
-    [92, 96, 88, 81, 68, 72, 76, 79],
+    [92, 96, 88, 81, 68, 72, 76, -1],
     [91, 95, 85, 82, 67, 71, 75, 80],
     [90, 94, 86, 83, 66, 70, 74, 78],
     [89, 93, 87, 84, 65, 69, 73, 77]
@@ -29,15 +41,8 @@ ledPinLocation = [
     [113, 117, 122, 125, 97, 101, 106, 110]
 ]
 
-startButtonPinLocation = 128
+startButtonPinLocation = 79
 wiringpi.pinMode(startButtonPinLocation, 0)
-
-# DECLARATION of wiring Pi
-wiringpi.wiringPiSetup()                    # initialise wiringpi
-wiringpi.mcp23017Setup(pin_base, ic1_addr)
-wiringpi.mcp23017Setup(82, ic2_addr)
-wiringpi.mcp23017Setup(98, ic3_addr)
-wiringpi.mcp23017Setup(115, ic3_addr)
 
 # Declaring button input as an i2C input
 for i in range(4):
@@ -45,25 +50,22 @@ for i in range(4):
         wiringpi.pinMode(buttonPinLocation[i][j], 0)
 
 # Declaring audio files wav etc
-crash = AudioSegment.from_wav("wav/crash.wav")
-hihat = AudioSegment.from_wav("wav/hihat.wav")
-kick = AudioSegment.from_wav("wav/kick.wav")
-snare = AudioSegment.from_wav("wav/snare.wav")
+pygame.mixer.pre_init(44100, -16, 1, 512)
+pygame.mixer.init()
 
-wavdict = {}
-for i in ["0", "1"]:
-    for j in ["0", "1"]:
-        for k in ["0", "1"]:
-            for l in ["0", "1"]:
-                wavdict[i+j+k +
-                        l] = AudioSegment.from_wav("combined/{}.wav".format(i+j+k+l))
+crash = pygame.mixer.Sound('wav/crash.wav')
+kick = pygame.mixer.Sound('wav/kick.wav')
+snare = pygame.mixer.Sound('wav/snare.wav')
+hihat = pygame.mixer.Sound('wav/hihat.wav')
+audiofiles = [crash, kick, snare, hihat]
+
 
 # Declaring misc variables
 timeoutSec = 300
 # to avoid start button being pressed again and turns everything off,
 # we set a timeout to atleast play some beats loop before the user can turn off the beats by pressing the start button again
 minimumSec = 2
-beatsPause = 400
+beatsPause = 200
 stopButtonPressed = False
 
 # Array for pressed or not pressed buttonsReading
@@ -76,35 +78,57 @@ buttonsReading = [
 
 
 # Play single beats
-def play_beats(row):
-    if (row == 0):
-        play(crash)
-    elif (row == 1):
-        play(hihat)
-    elif (row == 2):
-        play(kick)
-    elif (row == 3):
-        play(snare)
+def play_beat(row):
+    audiofiles[row].play()
+    time.sleep(1)
+
+def play_beat_diff_thread(row):
+    t3 = threading.Thread(target=play_beat, args=[row])
+    t3.start()
+
+def play_all_beats():
+    for i in range(8):
+        for j in range(4):
+            if buttonsReading[j][i]:
+                audiofiles[j].play()
+        time.sleep(beatsPause/1000)
 
 # Check for input and update the buttonsReading array  for BUTTONS
+
+flagPressedState = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+]
 
 
 def updateButtonsReading():
     global buttonsReading
+    global flagPressedState
     for i in range(4):
         for j in range(8):
             newReading = wiringpi.digitalRead(buttonPinLocation[i][j])
-            recentReading = buttonsReading[i][j]
-
+            #print(buttonsReading)
             # if recent/prev is 0 then new/next is 1, meaning button get turned on
-            if (recentReading == 0 and newReading == 1):
+            if ((buttonsReading[i][j] == 0) and (newReading == 1) and flagPressedState[i][j]== 0):
+                flagPressedState[i][j] = 1
                 buttonsReading[i][j] = 1
-                play_beats(i)
+                #threading
+                t3 = threading.Thread(target=play_beat, args=[i])
+                t3.start()
 
-            # if recent/prev is 1 then new/next is 0, meaning button get turned off
-            elif (recentReading == 1 and newReading == 0):
+            elif ((buttonsReading[i][j] == 1) and (newReading == 0)and flagPressedState[i][j]== 1):
+                #print("1,0,1")
+                flagPressedState[i][j] = 0
+
+            # if recent/prev is 1 then new/next is 1, meaning button get turned off
+            elif (buttonsReading[i][j] == 1 and newReading == 1 and flagPressedState[i][j]== 0 ):
+                flagPressedState[i][j]= 1 
                 buttonsReading[i][j] = 0
-                play_beats(i)
+
+            elif (buttonsReading[i][j] == 0 and newReading == 0 and flagPressedState[i][j]== 1 ):
+                flagPressedState[i][j]= 0 
 
             # if same then skip
             else:
@@ -114,7 +138,7 @@ def updateButtonsReading():
 # update the LED, if pressed then turns on, until it got pressed again ->then will turn off
 def updateLed():
     for i in range(4):
-        for i in range(8):
+        for j in range(8):
             wiringpi.digitalWrite(ledPinLocation[i][j], buttonsReading[i][j])
 
 
@@ -135,34 +159,20 @@ def turnOffAll():
             wiringpi.digitalWrite(ledPinLocation[i][j], 0)
 
 
-def compile_audio():
-    silence = AudioSegment.silent(duration=beatsPause*9, frame_rate=44100)
-    # loop through frames in the array, create an audio, overlay it to silent(nothing inside) -> an audio to play
-    for j in range(8):
-        frame_str = ""
-        for i in range(4):
-            frame_str += str(buttonsReading[i][j])
 
-        # string in each frame will look like something : frame_str="0011"
-        audiofile = wavdict[frame_str]
-        silence = silence.overlay(audiofile, position=beatsPause*(j+1))
-
-    return silence
-
-
-def play_beats(audio):
+def execute_beats():
     t = threading.Thread(target=sweepLED, args=[])
     t.start()
-    play(audio)
+    play_all_beats()
     t.join()
 
 
 def sweepLED():
-    for i in range(8):
-        for j in range(4):
+    for j in range(8):
+        for i in range(4):
             wiringpi.digitalWrite(ledPinLocation[i][j], buttonsReading[i][j])
         time.sleep(beatsPause/1000)
-        for j in range(4):
+        for i in range(4):
             wiringpi.digitalWrite(ledPinLocation[i][j], 0)
 
 
@@ -183,15 +193,17 @@ def pauseAll():
 
 
 while True:
+    #time.sleep(0.1)
     updateButtonsReading()
     updateLed()
+    #if (startButtonPressed()):
     if (startButtonPressed()):
+        #print("triggered")
         starttime = datetime.datetime.now()
 
         # make a separate thread for checking stopButtonPressed
         t2 = threading.Thread(target=checkStartButton, args=[starttime])
         t2.start()
-        compiledAudio = compile_audio()
 
         # this loop will run forever until 1)exceed 5 mins 2)start/stop button pressed again
         while True:
@@ -202,7 +214,7 @@ while True:
                 break
             if(stopButtonPressed):
                 break
-            play_beats(compiledAudio)
+            execute_beats()
 
         t2.join()
 
